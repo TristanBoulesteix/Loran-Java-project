32d8efb01bbd00e163ff370e4aa1eeb17676a108
cpt_n2mo <cpt_n2mo@10.169.130.26>
2018-05-29 15:47:07 +0200

    NEW CLASS IN CONTROLLER

--- controller/src/main/java/controller/PlayerMover.java
+++ controller/src/main/java/controller/PlayerMover.java
@@ -1,6 +1,6 @@
 package controller;
 
-public class PlayerMover implements Runable{ //Create
+public class PlayerMover implements Runable{ // Create the class PlayerMover wich is implements by Runable
 
 	public void PlayerMover() {
 		
