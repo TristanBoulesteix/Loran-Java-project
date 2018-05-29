c65f741d101c71559afb6a635cdda6fe30618306
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-05-29 17:33:48 +0200

    Some modifications on Model

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -1,23 +1,26 @@
 package model;
 
+import java.sql.SQLException;
+
 import model.component.Component;
+import model.dao.DTBReader;
 
 public class Model {
 	Component[][] components;
 
 	public Model() {
-
+		components = new Component[12][20];
 	}
 
 	public Component[][] getMap() {
-		return null;
+		return components;
 	}
 
-	private String readMap() {
-		return null;
+	private String readMap(int idMap) throws SQLException {
+		return DTBReader.ReturnMap(idMap);
 	}
 
-	private void generateMap(String map) {
+	public void generateMap() {
 
 	}
 }
