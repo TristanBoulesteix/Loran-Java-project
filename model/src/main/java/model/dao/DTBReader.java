b195f2ade463834fcf57b28480afef11b4e28595
cpt_n2mo <cpt_n2mo@10.169.130.26>
2018-05-29 13:54:37 +0200

    add class to DAO

---
+++ model/src/main/java/model/dao/DTBReader.java
@@ -0,0 +1,18 @@
+package model.dao;
+
+public class DTBReader extends AbstractDAO {
+	private final static String PROCEDURE_CALL;
+	
+	private static String DTBeader() {
+		return null;
+		
+	}
+	
+	public static String ReturnMap() {
+		return null;
+		
+	}
+	
+	
+
+}
---
+++ model/src/main/java/model/dao/SpriteR.java
@@ -0,0 +1,26 @@
+package model.dao;
+
+import java.awt.Image;
+import java.util.ArrayList;
+
+public class SpriteR {
+	public int Size;
+	private  ArrayList<Image> Sprites;
+	
+	public String SpriteR() {
+		return null;
+		
+	}
+	
+	private void ImageFromFileSysteme(){
+		
+	}
+	
+	public ArrayList<Image> getImage(){
+		return Sprites;
+		
+	}
+	
+	
+
+}
