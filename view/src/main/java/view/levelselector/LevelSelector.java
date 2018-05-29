9ca96c45405062cb39cda7e9541cb4ecf0906741
n2mo <cpt_n2mo@MacBook-Pro-de-n2mo.local>
2018-05-29 20:09:31 +0200

    LVL SELECTOR

--- view/src/main/java/view/levelselector/LevelSelector.java
+++ view/src/main/java/view/levelselector/LevelSelector.java
@@ -1,4 +1,5 @@
 package view.levelselector;
+import javax.swing.JOptionPane;
 
 public class LevelSelector {
 
@@ -8,12 +9,17 @@ public class LevelSelector {
 
 
 public LevelSelector() {
+
 	
 }
 
 public int getLevel() {
-	return level;
-
+	  String[] LEVEL_LIST = {"Map 1", "Map 2", "Map 3", "Map 4"};
+	    JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane(), jop3 = new JOptionPane(), jop4 = new JOptionPane();
+	    String nom = (String)jop.showInputDialog(null, 
+	      "Choose a Map !",
+	      "Loran-MapSelector",
+	      JOptionPane.QUESTION_MESSAGE, null,LEVEL_LIST,Map[2], LEVEL_LIST[3], LEVEL_LIST[4]);
 }
 
 }
