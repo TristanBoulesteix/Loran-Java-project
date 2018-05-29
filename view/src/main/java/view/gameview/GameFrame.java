6bab68e4b05517ba439f8b0a65c356effec65edd
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2018-05-29 16:28:09 +0200

    View initialize layout
    start

--- view/src/main/java/view/gameview/GameFrame.java
+++ view/src/main/java/view/gameview/GameFrame.java
@@ -1,17 +1,28 @@
 package view.gameview;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 import model.Model;
 import model.component.Component;
 
-public class GameFrame {
+public class GameFrame extends JFrame{
 
 	private GamePanel panel;
 	private Model model;
-
+	
 public GameFrame(Model model) {
 	
+	panel=new GamePanel(model);
+	this.setTitle("Lorann");
+	this.setSize(1000, 800);
+	this.setLocationRelativeTo(null);
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+	this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	this.setContentPane(panel);
+    this.setVisible(true);
+    this.setResizable(false);
 }
 	
 public void initializeMapComponent(Component[][] component) {
--- view/src/main/java/view/gameview/GamePanel.java
+++ view/src/main/java/view/gameview/GamePanel.java
@@ -1,18 +1,21 @@
 package view.gameview;
 
+import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import model.Model;
 import model.component.Component;
 
-public class GamePanel {
+
+public class GamePanel extends JPanel{
 	private JLabel[][] labels;
 	private Model model;
 
 	
 public GamePanel(Model model) {
-	
+	setLayout(new GridBagLayout());
 }
 	
 void gameview() {
