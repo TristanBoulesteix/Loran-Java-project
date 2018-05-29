9de0d0f07690da9d9dd37c97b220e64a06aa5674
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-05-29 21:20:39 +0200

    array of componets is generated
    I also add a save to keep in memory our original map. I don't know if it will usefull. I also update the factory to correspond to our needs.

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -2,25 +2,58 @@ package model;
 
 import java.sql.SQLException;
 
+import model.component.Arrbarr;
+import model.component.Cargyv;
 import model.component.Component;
+import model.component.ComponentFactory;
+import model.component.Kyracj;
+import model.component.Lorann;
+import model.component.Maarcg;
 import model.dao.DTBReader;
 
 public class Model {
 	Component[][] components;
+	Component[][] originalMap; // A map to keep a save of our the original position of the components
 
 	public Model() {
 		components = new Component[12][20];
+		originalMap = new Component[12][20];
 	}
 
 	public Component[][] getMap() {
 		return components;
 	}
 
-	private String readMap(int idMap) throws SQLException {
+	public Component[][] getOriginalMap() {
+		return originalMap;
+	}
+
+	private String getMapFromDTB(int idMap) throws SQLException {
 		return DTBReader.ReturnMap(idMap);
 	}
 
-	public void generateMap() {
+	public void generateMap(int idMap) throws SQLException {
+		String map = getMapFromDTB(idMap);
+		components = ComponentFactory.buildComponentsFromMap(map);
+		originalMap = generateOriginalMap(components);
+	}
 
+	private Component[][] generateOriginalMap(Component[][] components) {
+		Component[][] map = new Component[12][20];
+
+		for (int i = 0; i < components.length; i++) {
+			for (int j = 0; j < components[i].length; j++) {
+				Component element = components[i][j];
+
+				if ((element instanceof Lorann) || (element instanceof Arrbarr) || (element instanceof Cargyv)
+						|| (element instanceof Maarcg) || (element instanceof Kyracj)) {
+					map[i][j] = null;
+				} else {
+					map[i][j] = element;
+				}
+			}
+		}
+
+		return map;
 	}
 }
