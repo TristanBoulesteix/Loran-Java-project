103c60840c926111283f1a8686a6803138f1c9c0
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2018-05-29 13:52:13 +0200

    view
    add class to view

---
+++ view/src/main/java/view/gameview/GameFrame.java
@@ -0,0 +1,25 @@
+package view.gameview;
+
+import javax.swing.JFrame;
+
+public class GameFrame {
+
+	private GamePanel panel;
+	private Model model;
+
+public GameFrame(model Model) {
+	
+}
+	
+public initializeMapComponent(component Component[][]) {
+	
+}
+
+public GamePanel getPanel() {
+	return panel;
+}
+
+	
+}
+
+
