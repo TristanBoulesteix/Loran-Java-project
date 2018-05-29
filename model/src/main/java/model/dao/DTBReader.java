8881be083bdebedc4ccb12d1b9a50079efc54034
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-05-29 15:05:32 +0200

    Temporary deleting error

--- model/src/main/java/model/dao/DTBReader.java
+++ model/src/main/java/model/dao/DTBReader.java
@@ -1,16 +1,15 @@
 package model.dao;
 
 public class DTBReader extends AbstractDAO {
-	private static final String PROCEDURE_CALL;
-	
+	private static final String PROCEDURE_CALL = "";
+
 	private String DTBReader() {
 		return null;
-		
+
 	}
-	
-	
+
 	public String ReturnMap() {
 		return null;
-		
+
 	}
 }
