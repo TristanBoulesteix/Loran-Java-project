9458a610c40ff6abdd64a14a850ec9597549cbb1
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-05-30 11:46:40 +0200

    Correction of the popup
    The textField became a comboBox

--- view/src/main/java/view/levelselector/LevelSelector.java
+++ view/src/main/java/view/levelselector/LevelSelector.java
@@ -1,4 +1,5 @@
 package view.levelselector;
+
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -6,44 +7,41 @@ public class LevelSelector {
 
 	private int level;
 	private static String[] LEVEL_LIST;
-	
 
+	public LevelSelector() {
 
-public LevelSelector() {
+	}
 
-	
-}
+	public static int getLevel() {
+		String[] LEVEL_LIST = { "Map 1", "Map 2", "Map 3", "Map 4" };
+		String nom = (String) JOptionPane.showInputDialog(null, "Choose a Map!", "Lorann-MapSelector",
+				JOptionPane.QUESTION_MESSAGE, null, LEVEL_LIST, LEVEL_LIST[0]);
 
-public static int getLevel() {
-	  String[] LEVEL_LIST = {"Map 1", "Map 2", "Map 3", "Map 4"};
-	    String nom = (String)JOptionPane.showInputDialog(null, "Choose a Map!", "Lorann-MapSelector",JOptionPane.OK_CANCEL_OPTION);
-		
-	    if (nom.equals(null)) {
-	    	System.exit(0);
+		if (nom.equals(null)) {
+			System.exit(0);
 		}
-	    switch (nom) {
+		switch (nom) {
 		case "Map 1":
 			return 1;
-			
+
 		case "Map 2":
 			return 2;
-			
+
 		case "Map 3":
 			return 3;
-			
+
 		case "Map 4":
 			return 4;
-			
+
 		case "Map 5":
 			return 5;
-			
 
 		default:
-			
+
 			final JPanel panel = new JPanel();
-			 JOptionPane.showMessageDialog(panel, "Invalid choice !", "Error", JOptionPane.ERROR_MESSAGE);
-			 return 1;
+			JOptionPane.showMessageDialog(panel, "Invalid choice !", "Error", JOptionPane.ERROR_MESSAGE);
+			return 1;
 		}
-}
+	}
 
 }
