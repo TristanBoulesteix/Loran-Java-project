7ba7d23c827f0a3018ceb370946ccc27602305df
n2mo <cpt_n2mo@MacBook-Pro-de-n2mo.local>
2018-05-30 09:48:50 +0200

    LEVEL SELECTOR BY ZAC + PACKAGE DIAGRAME BY JP KARDOUS

--- view/src/main/java/view/levelselector/LevelSelector.java
+++ view/src/main/java/view/levelselector/LevelSelector.java
@@ -1,5 +1,6 @@
 package view.levelselector;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class LevelSelector {
 
@@ -15,11 +16,34 @@ public LevelSelector() {
 
 public int getLevel() {
 	  String[] LEVEL_LIST = {"Map 1", "Map 2", "Map 3", "Map 4"};
-	    JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane(), jop3 = new JOptionPane(), jop4 = new JOptionPane();
-	    String nom = (String)jop.showInputDialog(null, 
-	      "Choose a Map !",
-	      "Loran-MapSelector",
-	      JOptionPane.QUESTION_MESSAGE, null,LEVEL_LIST,Map[2], LEVEL_LIST[3], LEVEL_LIST[4]);
+	    String nom = (String)JOptionPane.showInputDialog(null, "Choose a Map!", "Lorann-MapSelector",JOptionPane.OK_CANCEL_OPTION);
+		
+	    if (nom.equals(null)) {
+	    	System.exit(0);
+		}
+	    switch (nom) {
+		case "Map 1":
+			return 1;
+			
+		case "Map 2":
+			return 2;
+			
+		case "Map 3":
+			return 3;
+			
+		case "Map 4":
+			return 4;
+			
+		case "Map 5":
+			return 5;
+			
+
+		default:
+			
+			final JPanel panel = new JPanel();
+			 JOptionPane.showMessageDialog(panel, "Invalid choice !", "Error", JOptionPane.ERROR_MESSAGE);
+			 return 1;
+		}
 }
 
 }
