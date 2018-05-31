6a7483a2beaf41839652a7b15eddb4ef1b9b30a7
n2mo <cpt_n2mo@MacBook-Pro-de-n2mo.local>
2018-05-31 09:46:03 +0200

    supretion

--- controller/src/main/java/controller/DemonMover.java
+++ controller/src/main/java/controller/DemonMover.java
@@ -8,27 +8,8 @@ public class DemonMover implements Runnable { // Create the class DemonMover wic
 	private Direction direction; // Create the attribute direction
 
 	public DemonMover(Component[][] componementsPosition) { // Demons Movement
-		/* Point pos = monster.getPos().getLocation();
-	        Point nextPos = this.computeNextPos(
-	                monster.getDirection(
-	                        this.hero.getPos().getLocation(),
-	                        this.tileMap),
-	                pos);
-	        if(nextPos != pos) {
-	            String element = tileMap[nextPos.x][nextPos.y].getClass()
-	                    .getSimpleName();
-	            if(element.contains("Hero")) {
-	                this.dead = true;
-	            } else if(!element.contains("Monster") &&
-	                    !element.contains("Purse") &&
-	                    !element.contains("Crystal") &&
-	                    !element.contains("Door")) {
-	                tileMap[pos.x][pos.y] = model.element(' ', pos.getLocation());
-	                monster.setLocation(nextPos);
-	                tileMap[nextPos.x][nextPos.y] = monster;
-	            }
-	        }
-	}*/ //ID demon mover
+
+	}
 
 	public Component[][] getComponementsPosition() { // Access to variables
 		return null;
--- controller/src/main/java/controller/PlayerMover.java
+++ controller/src/main/java/controller/PlayerMover.java
@@ -3,35 +3,7 @@ package controller;
 public class PlayerMover implements Runnable { // Create the class PlayerMover wich is implements by Runable
 
 	public void PlayerMover() {
-        /*
-		Point pos = this.hero.getPos();
-        this.hero.move(order, tileMap, this.view);
-        this.tileMap[pos.x][pos.y] = model.element(' ', pos.getLocation());
-
-        pos = this.hero.getPos();
-        String elementName = this.tileMap[pos.x][pos.y].getClass().getSimpleName();
-        if(elementName.contains("Monster")) {
-            this.dead = true;
-        } else if (elementName.contains("Crystal") && this.posDoor != null) {
-            this.tileMap[this.posDoor.x][this.posDoor.y] = model.element('O', this.posDoor);
-            this.score += 100;
-        } else if (elementName.contains("OpenDoor")) {
-            this.level++;
-            if(this.level > 9) {
-                this.model.upNameAndScore(this.score, this.pseudo);
-                this.view.printMessage(String.format("WELL DONE %s! Your score is : %d\nPress OK to restart the game", this.pseudo, this.score));
-                this.score = 0;
-                this.level = 1;
-            }
-            this.model.loadMap(String.format("MAP%d", this.level));
-            return;
-        } else if (elementName.contains("Purse")) {
-            this.score += 250;
-        }
-        this.tileMap[pos.x][pos.y] = this.hero;
-        this.view.repaint();
-        */
-		//ID move for player
+        
 	}
 
 	@Override
