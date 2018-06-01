195ff76787d1e09d6ed847356b9a69794dabc4ed
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-01 16:54:36 +0200

    Add JavaDoc to Spell

--- model/src/main/java/model/component/Spell.java
+++ model/src/main/java/model/component/Spell.java
@@ -1,8 +1,18 @@
 package model.component;
-
+/**
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ *
+ */
 public class Spell extends Touch implements Kill {
 	private final static String PATH = "Pictures/spell";
-
+/**
+ * 
+ * @param lorannPermeability
+ * @param demonPermeability
+ * @param coordinate
+ * @param available
+ */
 	public Spell(boolean lorannPermeability, boolean demonPermeability, Coordinate coordinate, boolean available) {
 		super(PATH, lorannPermeability, demonPermeability, coordinate, available, Direction.UP);
 	}
