be7f22080c6bf1407467044d977697d6ab805447
n2mo <cpt_n2mo@MacBook-Pro-de-n2mo.local>
2018-06-01 09:58:41 +0200

    updates of demonMover

--- controller/src/main/java/controller/DemonMover.java
+++ controller/src/main/java/controller/DemonMover.java
@@ -1,15 +1,35 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import model.component.Component;
+import model.component.Demon;
 import model.component.Direction;
 
 public class DemonMover { // Create the class DemonMover wich is implements by Runable
 
-	private Direction direction; // Create the attribute direction
-
-	public DemonMover(Component[][] componementsPosition) { // Demons Movement
+	private Demon demon; // Create the attribute direction
+	private Controller controller;
+	
+	public DemonMover(Demon demon, Controller controller) { // Demons Movement
+		this.demon = demon;
+		this.controller = controller;
 
 	}
+	
+	public void move() {
+	//methode move appel d'autre methode crer un nbr aléatoir
+	Random rand = new Random();
+	
+	//get toute les position possible puis arraylist de enum
+	ArrayList<Direction> list = Stream.of(Direction.values());
+	
+	//apel de la methode moveC du contructeur
+	controller.moveComponent(demon, direction);
+	}
+
 
 	public Component[][] getComponementsPosition() { // Access to variables
 		return null;
