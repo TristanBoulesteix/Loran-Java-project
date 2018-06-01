726e71b7457b5645d061f17ac7e011f607f6763f
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-01 16:57:39 +0200

    Add JavaDoc to Treasure

--- model/src/main/java/model/component/Treasure.java
+++ model/src/main/java/model/component/Treasure.java
@@ -1,13 +1,26 @@
 package model.component;
-
+/**
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ *
+ */
 public class Treasure extends Touch {
 	private final static String PATH = "Pictures/treasure";
 	private final static int VALUE = 10;
-
+/**
+ * 
+ * @param lorannPermeability
+ * @param demonPermeability
+ * @param coordinate
+ * @param available
+ */
 	public Treasure(boolean lorannPermeability, boolean demonPermeability, Coordinate coordinate, boolean available) {
 		super(PATH, lorannPermeability, demonPermeability, coordinate, available, Direction.UP);
 	}
-
+/**
+ * 
+ * @return Value
+ */
 	public int getValue() {
 		return VALUE;
 	}
