2b2a0b7c737fc931c1298fa8b47676f67f123838
Nxtrik <33619208+Nxtrik@users.noreply.github.com>
2018-06-01 14:08:43 +0200

    commentary View

--- view/src/main/java/view/gameview/GameFrame.java
+++ view/src/main/java/view/gameview/GameFrame.java
@@ -9,24 +9,40 @@ import javax.swing.JFrame;
 import model.Model;
 import view.IGameFrame;
 
+
+
 @SuppressWarnings("serial")
 public class GameFrame extends JFrame implements IGameFrame {
-	private GamePanel panel;
+	private GamePanel panel;	
 	private Model model;
 
-	public GameFrame(Model model, Observer observer) {
+	
+	
+	public GameFrame(Model model, Observer observer)	{
+	/*We create a Frame with parameters*/	 
 		this.panel = new GamePanel(model, observer);
+	/*we put the Panel attentive to the events*/
 		this.panel.setFocusable(true);
+	/*prevent the creation of a shortcut bar at the top of the screen*/
 		this.setUndecorated(true);
+	/*We put the title of the game on the Frame*/	
 		this.setTitle("Lorann");
+	/*We put the Frame on the center of the screen*/	
 		this.setLocationRelativeTo(null);
+	/*We close the processus when we close the window*/
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/*We put the Frame on full screen*/
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	/*We put the background in black*/
 		this.getContentPane().setBackground(Color.BLACK);
+	/*We put the Panel on the center of the Frame*/
 		this.getContentPane().add(panel, BorderLayout.CENTER);
+	/*we forbid resizing*/
 		this.setResizable(false);
 	}
 
+	
+	
 	public GamePanel getPanel() {
 		return panel;
 	}
