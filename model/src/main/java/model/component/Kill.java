511791c7ed9ef9d98560619393f5d5acc1940ddb
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-01 16:48:02 +0200

    Add JavaDoc to Kill

--- model/src/main/java/model/component/Kill.java
+++ model/src/main/java/model/component/Kill.java
@@ -1,5 +1,10 @@
 package model.component;
-
+/**
+ * Component target
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ *
+ */
 public interface Kill {
 	public void kill(Component target);
 }
