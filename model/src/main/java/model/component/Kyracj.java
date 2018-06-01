94059730ef04bb76a9342f0e6867fae5dfd8617f
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-01 16:48:58 +0200

    Add JavaDoc to Kyracj

--- model/src/main/java/model/component/Kyracj.java
+++ model/src/main/java/model/component/Kyracj.java
@@ -1,8 +1,18 @@
 package model.component;
-
+/**
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ *
+ */
 public class Kyracj extends Demon {
 	private final static String PATH = "Pictures/demons/kyracj";
-
+/**
+ * 
+ * @param lorannPermeability
+ * @param demonPermeability
+ * @param coordinate
+ * @param direction
+ */
 	public Kyracj(boolean lorannPermeability, boolean demonPermeability, Coordinate coordinate, Direction direction) {
 		super(PATH, lorannPermeability, demonPermeability, coordinate, direction);
 	}
