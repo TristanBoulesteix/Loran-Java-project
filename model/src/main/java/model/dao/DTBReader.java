5a2561ac8482c4aecb24c052b0d3579b3aa480ae
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-01 16:59:24 +0200

    Add JavaDoc to DTBReader

--- model/src/main/java/model/dao/DTBReader.java
+++ model/src/main/java/model/dao/DTBReader.java
@@ -3,10 +3,19 @@ package model.dao;
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+/**
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ *
+ */
 public class DTBReader extends AbstractDAO {
 	private static final String PROCEDURE_CALL = "{CALL findMapByID(?)}";
-
+/**
+ * 
+ * @param id
+ * @return
+ * @throws SQLException
+ */
 	public static String ReturnMap(int id) throws SQLException {
 		//Call stored procedure in SQL
 		final CallableStatement callStatement = prepareCall(PROCEDURE_CALL);
