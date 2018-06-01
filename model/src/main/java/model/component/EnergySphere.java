3744d3c9a380ee9a1e9e63e8c59c3c98adf4d0d0
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-01 16:44:30 +0200

    Add JavaDoc to EnergySphere

--- model/src/main/java/model/component/EnergySphere.java
+++ model/src/main/java/model/component/EnergySphere.java
@@ -1,8 +1,18 @@
 package model.component;
-
+/**
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ *
+ */
 public class EnergySphere extends Touch {
 	private final static String PATH = "Pictures/energySphere";
-
+/**
+ * 
+ * @param lorannPermeability
+ * @param demonPermeability
+ * @param coordinate
+ * @param available
+ */
 	public EnergySphere(boolean lorannPermeability, boolean demonPermeability, Coordinate coordinate,
 			boolean available) {
 		super(PATH, lorannPermeability, demonPermeability, coordinate, available, Direction.UP);
