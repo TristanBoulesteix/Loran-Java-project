552db50ebadc6f574493f54521ae870ffe349cfb
jeanpierrekardous <jeanpierre.kardous@viacesi.fr>
2018-06-02 11:46:44 +0200

    Add JavaDoc to SpriteReader

--- model/src/main/java/model/dao/SpriteReader.java
+++ model/src/main/java/model/dao/SpriteReader.java
@@ -7,11 +7,21 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+/**
+ * 
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean
+ *         Pierre
+ *
+ */
 public class SpriteReader {
 	public static final int SIZE = 0;
 	private ArrayList<Image> sprites;
 	private String path;
 
+	/**
+	 * 
+	 * @param path
+	 */
 	public SpriteReader(String path) {
 		this.path = path;
 		this.sprites = new ArrayList<Image>();
@@ -22,17 +32,25 @@ public class SpriteReader {
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * 
+	 * @throws IOException
+	 */
 	private void generateImagesList() throws IOException {
-		//Get images list from file system
+		// Get images list from file system
 		File folder = new File(getClass().getClassLoader().getResource(path).getFile()).getAbsoluteFile();
 		File[] listOfFiles = folder.listFiles();
-		//Add Images in ArrayList
+		// Add Images in ArrayList
 		for (int i = 0; i < listOfFiles.length; i++) {
 			sprites.add(ImageIO.read(listOfFiles[i].getAbsoluteFile()));
 		}
 	}
 
+	/**
+	 * 
+	 * @return sprites
+	 */
 	public ArrayList<Image> getSprites() {
 		return sprites;
 	}
