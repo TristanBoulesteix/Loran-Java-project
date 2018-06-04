7c1a7056b444adf32a6e0a5d1929998d844d7a6b
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-04 09:05:08 +0200

    Lorann sprites in function of the direction

--- view/src/main/java/view/gameview/GamePanel.java
+++ view/src/main/java/view/gameview/GamePanel.java
@@ -10,57 +10,62 @@ import javax.swing.JPanel;
 import controller.IController;
 import controller.Order;
 import model.Model;
-import model.component.Direction;
 import model.component.IComponent;
 import view.IGamePanel;
+
 /**
  * 
- * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ * @author BOULSTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean
+ *         Pierre
  *
  */
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel implements IGamePanel, KeyListener {
 	private IComponent[][] components;
 	private IController controller;
-/**
- * 
- * @param model
- * @param controller
- */
+
+	/**
+	 * 
+	 * @param model
+	 * @param controller
+	 */
 	public GamePanel(Model model, IController controller) {
 		this.addKeyListener(this);
 		this.controller = controller;
 		this.setBackground(Color.BLACK);
 	}
-/**
- * We update the map and we repaint 
- */
+
+	/**
+	 * We update the map and we repaint
+	 */
 	public void updateMap(IComponent[][] components) {
-		
+
 		this.components = components;
 		repaint();
 	}
-/**
- * We enlarge the images to put the game in full screen 
- */
+
+	/**
+	 * We enlarge the images to put the game in full screen
+	 */
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		for (int x = 0; x < components.length; x++) {
 			for (int y = 0; y < components[x].length; y++) {
-				g.drawImage(components[x][y].getImage(Direction.LEFT), y * 91, x * 91, 80, 80, null);
+				g.drawImage(components[x][y].getImage(components[x][y].getDirection()), y * 91, x * 91, 80, 80, null);
 			}
 		}
 
 	}
-/**
- * we inform our program of the action of certain keys on the keyboard 
- */
+
+	/**
+	 * we inform our program of the action of certain keys on the keyboard
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-	 
+
 		switch (keyCode) {
 		case KeyEvent.VK_UP:
 			controller.setAction(Order.UP);
@@ -87,17 +92,19 @@ public class GamePanel extends JPanel implements IGamePanel, KeyListener {
 		}
 
 	}
-/**
- * 
- */
+
+	/**
+	 * 
+	 */
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 
 	}
-/**
- * 
- */
+
+	/**
+	 * 
+	 */
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
