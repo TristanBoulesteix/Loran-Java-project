a4d0aaead26c5c5e5fa15b332d5036cebc6ecaae
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-05 11:18:44 +0200

    Components tests done

--- model/src/test/java/model/component/ComponentTest.java
+++ model/src/test/java/model/component/ComponentTest.java
@@ -1,11 +1,15 @@
 package model.component;
 
+import static org.junit.Assert.assertFalse;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class ComponentTest {
+	private Lorann lorann;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -17,10 +21,15 @@ public class ComponentTest {
 
 	@Before
 	public void setUp() throws Exception {
+		lorann = new Lorann(false, true, new Coordinate(5, 5), Direction.UP);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	@Test
+	public void isLORANN_PERMEABLE() {
+		assertFalse(null, lorann.isLORANN_PERMEABLE());
+	}
 }
