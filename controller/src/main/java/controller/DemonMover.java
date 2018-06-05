7b4f650ca7100ce0cf05f3984e3670635d1ae869
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-06-05 10:53:01 +0200

    Delete unused method on DemonMover

--- controller/src/main/java/controller/DemonMover.java
+++ controller/src/main/java/controller/DemonMover.java
@@ -9,7 +9,9 @@ import model.component.Direction;
 
 /**
  * <h1>Create the class DemonMover which is implements by Runnable.</h1>
- * @author BOULESTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean Pierre
+ * 
+ * @author BOULESTEIX Tristan, MAITRE Maxime, AZZOUZI Zacharia, KARDOUS Jean
+ *         Pierre
  *
  */
 public class DemonMover {
@@ -18,46 +20,34 @@ public class DemonMover {
 
 	/**
 	 * Operation call to move Demon.
+	 * 
 	 * @param demon
-	 * The demon.
+	 *            The demon.
 	 * @param controller
-	 * The controller.
+	 *            The controller.
 	 */
 	public DemonMover(Demon demon, Controller controller) {
 		this.demon = demon;
 		DemonMover.controller = controller;
 
 	}
-/** 
- * Call of MoveComponent.
- * get the enum in an ArrayList and use it randomly.
- *  Creation of the new Random.
- * 
- */
+
+	/**
+	 * Call of MoveComponent. get the enum in an ArrayList and use it randomly.
+	 * Creation of the new Random.
+	 * 
+	 */
 	public void move() {
 		Random random = new Random();
 		ArrayList<Direction> list = new ArrayList<Direction>(EnumSet.allOf(Direction.class));
 		int index = random.nextInt(list.size());
 		controller.moveComponent(demon, list.get(index));
 	}
-/**Get the enum in an ArrayList and use it randomly.
- * 	Creation of the new Random.
- *  Call of MoveComponent.
- * @param demonToMove
- * The Demon to move.
- */
-	public static void move(Demon demonToMove) {
-	
-		Random random = new Random();
-		ArrayList<Direction> list = new ArrayList<Direction>(EnumSet.allOf(Direction.class));
-		int index = random.nextInt(list.size());
-		controller.moveComponent(demonToMove, list.get(index));
-	}
 
 	/**
 	 * Operation call to get a Demon.
-	 * @return demon 
-	 * The demon.
+	 * 
+	 * @return demon The demon.
 	 */
 	public Demon getDemon() {
 		return demon;
